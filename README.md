b7e0d2e10a01bf6c765ec559b9a287c7a7d5165c
Markus Näther <naether.markus@gmail.com>
2020-07-15 14:06:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ArXiv Downloader
+
+Just install via ```python setup.py install --user```.
+
+There are then three programs available:
+
+```
+arxiv_fetcher: Recieve a list of all available papers on arxiv
+arxiv_autoload: Downloads all papers within a given txt file (usually works on the resulting txt of the above step)
+arxiv_downloader: Downloads a single, given, paper by its id
+```
---
+++ arxiv_downloader/__main__.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019-2020, University of Freiburg.
+# Chair of Algorithms and Data Structures.
+# Markus Näther <naetherm@informatik.uni-freiburg.de>
+
+import sys
+import argparse
+
+from arxiv_downloader import ArXiv, ArXivPaper
+
+
+def main(argv=None):
+
+  if argv == None:
+    argv = sys.argv[1:]
+
+  parser = argparse.ArgumentParser(prog="arxiv_downloader", add_help=False)
+
+  parser.add_argument(
+    "--paper-id",
+    dest="paper_id",
+    type=str,
+    required=True,
+    help="The ID of the paper to download"
+  )
+  parser.add_argument(
+    "--download-dir",
+    dest="download_dir",
+    type=str,
+    required=True,
+    help="The directory where to download the file."
+  )
+
+  args = parser.parse_args()
+
+  ArXiv.download_by_pid(args.paper_id, args.download_dir)
+
+def fetch_main():
+  ArXiv.fetch_papers()
+
+def autoload_main(argv=None):
+
+  if argv == None:
+    argv = sys.argv[1:]
+
+  parser = argparse.ArgumentParser(prog="arxiv_autoload", add_help=False)
+  parser.add_argument(
+    "--paper-ids",
+    dest="paper_ids",
+    type=str,
+    required=True,
+    help="Path to the file holding all paper ids to download."
+  )
+  parser.add_argument(
+    "--download-dir",
+    dest="download_dir",
+    type=str,
+    required=True,
+    help="The directory where to download the file."
+  )
+
+  args = parser.parse_args()
+
+  with open(args.paper_ids, 'r') as fin:
+    for lin in fin:
+      lin = lin.replace('\n', '')
+      print("Downloading ArXiv paper: {}".format(lin))
+      ArXiv.download_by_pid(lin, args.download_dir)
+
+if __name__ == '__main__':
+  main()
